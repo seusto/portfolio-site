152eff8dbaaf93d9e5ebdfef3d4781fcbdf0d730
sermarino <sergiofmarino@gmail.com>
2025-10-28 19:57:50 +0100

    primo commit dal nuovo account

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen grid place-items-center bg-gray-50">
+      <section className="py-16">
+        <h1>Integration & Full-Stack Developer </h1>
+      </section>
+      <button className="px-4 py-2 rounded-lg border hover:bg-gray-100">
+        Ciao Mondo!! 👋
+      </button>
+      <button className="px-4 py-2 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700">
+  Salva
+</button>
+    </main>
+  );
+}
