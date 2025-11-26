3b0cf59bb1e5bac939481d60550de78a0b905c92
seusto <sergiopleiax@gmail.com>
2025-11-27 00:37:38 +0100

    testo for commit

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -4,6 +4,6 @@ export const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE!,
     {
-        auth: { persistSession: false },
+        auth: { persistSession: false }, 
     }
 );
