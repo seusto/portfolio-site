69bdb65313e1d16ba4d41e58c466c4c04d9fa05b
seusto <sergiopleiax@gmail.com>
2025-11-27 01:02:47 +0100

    new test for pushing

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE!,
+    process.env.SUPABASE_SERVICE_ROLE!, 
     {
         auth: { persistSession: false }, 
     }
