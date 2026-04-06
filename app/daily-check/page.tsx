5628ede4dd0e6d49f868a8945d582cdd5fb0f0b4
seusto <sergiopleiax@gmail.com>
2026-04-07 00:33:26 +0200

    error fix

--- app/daily-check/page.tsx
+++ app/daily-check/page.tsx
@@ -5,7 +5,7 @@ import Calendar from "@/components/daily-check/Calendar";
 export default function DailyCheckPage(){
     return(
         <div>
-            Sergio's today
+            Sergios today
             <Calendar
             onChange={(d) => console.log("Selezionato:", d.toISOString().slice(0,10))}
             className="rounded-3xl bg-neutral-50 p-3 dark:bg-neutral-900"
